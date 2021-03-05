7a73d36b85cd1c37ac9707df1bceacaa0d31e2e8
Diego Cardenas <A1227808@gmail.com>
2021-03-04 22:24:30 -0600

    project setup

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <ThreeTest/>
+  </div>
+</template>
+
+<script>
+import ThreeTest from './components/ThreeTest'
+
+export default {
+  name: 'App',
+  components: {
+    ThreeTest
+  }
+}
+</script>
