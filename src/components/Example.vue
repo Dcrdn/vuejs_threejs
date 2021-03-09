818916df4c82e703c624dac16d55b375c1fe1534
Diego Cardenas <A1227808@gmail.com>
2021-03-08 22:29:38 -0600

    Shapes and menu conf added

--- src/components/Example.vue
+++ src/components/Example.vue
@@ -1,144 +1,189 @@
 <template>
   <div id="container"></div>
 </template>
-<script>
+<script type ='module'>
+// import * as THREE from 'https://unpkg.com/three/build/three.module.js';
+// import { OrbitControls } from 'https://unpkg.com/three/examples/jsm/controls/OrbitControls.js';
+// import Stats from "https://unpkg.com/three/examples/jsm/libs/stats.module.js";
+// import dat from "https://unpkg.com/three/examples/jsm/libs/dat.gui.module.js";
 import * as THREE from 'three'
-import { OrbitControls } from './jsm/controls/OrbitControls.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import Stats from "three/examples/jsm/libs/stats.module.js";
 import dat from 'three/examples/jsm/libs/dat.gui.module.js';
-// import Stats from "./jsm/libs/stats.module.js";
-// import dat from './jsm/libs/dat.gui.module.js';
 
 export default {
   name: 'Example',
   data() {
     return {
-      cube: null,
       renderer: null,
       scene: null,
       camera: null,
-      mesh: null,
-      stats: null,
       cameraControls: null,
+      mesh_cube: null,
+      mesh_sphere: null,
+      mesh_cone: null,
+      stats: null,
     }
   },
   methods: {
     init: function() {
-        // Renderer engine
-        this.renderer = new THREE.WebGLRenderer({antialias: true});
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
-        document.body.appendChild(this.renderer.domElement);
+      // RENDERER ENGINE
+      this.renderer = new THREE.WebGLRenderer({antialias: true});
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
+      document.body.appendChild(this.renderer.domElement);
 
-        // Scene
-        this.scene = new THREE.Scene();
+      // SCENE
+      this.scene = new THREE.Scene();
 
-        // Camera
-        let fov = 60;
-        let aspect = window.innerWidth / window.innerHeight;
-        let near = 0.1;
-        let far = 10000;
-        this.camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-        this.camera.position.set(0, 0, 3);
-        this.cameraControls = new OrbitControls(this.camera, this.renderer.domElement);
+      // CAMERA
+      let fov = 60;
+      let aspect = window.innerWidth / window.innerHeight;
+      let near = 0.1;
+      let far = 10000;
+      this.camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+      this.camera.position.set(0, 0, 8);
+      this.cameraControls = new OrbitControls(this.camera, this.renderer.domElement);
 
-        // MODELS
-        let geometry = new THREE.BoxGeometry();
-        let material = new THREE.MeshBasicMaterial({color: "white", wireframe: true});
-        this.mesh = new THREE.Mesh(geometry, material);
-        this.mesh.name = "Cube";
+      // MODELS
+      let geometry = new THREE.BoxGeometry();
+      let material = new THREE.MeshBasicMaterial({color: "white", wireframe: true});
+      this.mesh_cube = new THREE.Mesh(geometry, material);
+      this.mesh_cube.name = "Cube";
 
-        // SCENE GRAPH
-        this.scene.add(this.mesh);
-        this.scene.add(this.mesh);
-        // GUI
-        let gui = new dat.GUI();
+      // Sphere
+      let geometry_sphere = new THREE.SphereGeometry();
+      let material_sphere = new THREE.MeshBasicMaterial({color: "red", wireframe: true});
+      this.mesh_sphere = new THREE.Mesh(geometry_sphere, material_sphere);
+      this.mesh_sphere.name = "Sphere";
 
-        // model
-        let model = {
-            rotY: this.mesh.rotation.y * Math.PI / 180,
-            posHome: function() {
-                this.mesh.position.x = 0;
-            },
-            rotHome: function() {
-                model.rotY = 0;
-                this.mesh.rotation.y = 0;
-            }
-        }
+      // Cone
+      let geometry_cone = new THREE.ConeGeometry();
+      let material_cone = new THREE.MeshBasicMaterial({color: "yellow", wireframe: true});
+      this.mesh_cone = new THREE.Mesh(geometry_cone, material_cone);
+      this.mesh_cone.name = "Cone";
 
-        // General Info Menu
-        let generalMenu = gui.addFolder("General Info Menu");
-        let txfMeshName = generalMenu.add(this.mesh, "name").name("Model's Name")
-        .onChange(function(event) {
+      // SCENE GRAPH
+      this.scene.add(this.mesh_cube);
+      this.scene.add(this.mesh_sphere);
+      this.scene.add(this.mesh_cone);
 
-        }).onFinishChange(function(event) {
+      // Initial positions
+      this.mesh_cube.position.x = -3;
+      this.mesh_sphere.position.x = 0;
+      this.mesh_cone.position.x = 3;
 
-        });
+      // GUI
+      let gui = new dat.GUI();
 
-        // Position Menu
-        let positionMenu = gui.addFolder("Model's Position Menu");
-        let sliderPosX = positionMenu.add(this.mesh.position, "x").min(-5).max(5).step(0.5).setValue(0).name("X").listen().onChange(function(value) {
-        
-        });
-        let btnPosHome = positionMenu.add(model, "posHome").name("HOME").onChange(function(event) {
+      // model.cube
+      let model = {
+          cube: {
+              rotY: this.mesh_cube.rotation.y * Math.PI / 180,
+              default_params: function(){
+                  model.cube.rotY = 0;
+                  this.mesh_cube.rotation.y = 0;
+                  this.mesh_cube.position.x = -3;
+                  this.mesh_cube.material.color = new THREE.Color("white");
+                  model.cube.colorPalette = [1,1,1];
+                  this.mesh_cube.material.wireframe = true;
+              },
+              listColors: ["White", "Red", "Yellow"],
+              defaultItem: "White",
+              colorPalette: [1, 1, 1],
+              mesh: this.mesh_cube,
+              initial_x: -3
+          },
+          sphere: {
+              rotY: this.mesh_sphere.rotation.y * Math.PI / 180,
+              default_params: function(){
+                  model.sphere.rotY = 0;
+                  this.mesh_sphere.rotation.y = 0;
+                  this.mesh_sphere.position.x = 0;
+                  this.mesh_sphere.material.color = new THREE.Color("red");
+                  model.sphere.colorPalette = [1,1,1];
+                  this.mesh_sphere.material.wireframe = true;
+              },
+              listColors: ["White", "Red", "Yellow"],
+              defaultItem: "Red",
+              colorPalette: [1, 1, 1],
+              mesh: this.mesh_sphere,
+              initial_x: 0
+          },
+          cone: {
+              rotY: this.mesh_cone.rotation.y * Math.PI / 180,
+              default_params: function(){
+                  model.cone.rotY = 0;
+                  this.mesh_cone.rotation.y = 0;
+                  this.mesh_cone.position.x = 3;
+                  this.mesh_cone.material.color = new THREE.Color("yellow");
+                  model.cone.colorPalette = [1,1,1];
+                  this.mesh_cone.material.wireframe = true;
+              },
+              listColors: ["White", "Red", "Yellow"],
+              defaultItem: "Yellow",
+              colorPalette: [1, 1, 1],
+              mesh: this.mesh_cone,
+              initial_x: 3
+          }
+      }
+      // view & controller
+      
+      let cubeMenu = gui.addFolder("Cube");
+      let sphereMenu = gui.addFolder("Sphere");
+      let coneMenu = gui.addFolder("Cone");
+      let menus = ['cube', 'sphere', 'cone']
 
-        });
-        
-        // Orientation Menu
-        let rotMenu = gui.addFolder("Model's Rotation Menu");
-        let sliderRotY = rotMenu.add(model, "rotY").min(-180).max(180).step(10).setValue(0).name("Y (deg)").listen().onChange(function(value) {
-            this.mesh.rotation.y = model.rotY * Math.PI / 180;
-        });;
+      let object_mapping = {
+          'cube': cubeMenu,
+          'sphere': sphereMenu,
+          'cone': coneMenu
+      }
 
-        let btnRotHome = rotMenu.add(model, "rotHome").name("HOME").onChange(function(event) {
+      // Menu items
+      for(let i = 0; i < menus.length ; i++){
+          let current_shape = menus[i];
+          let current_menu = object_mapping[current_shape];
+          let current_model = model[current_shape];
+          let current_mesh = current_model.mesh;
+          
+          let txfMeshName = current_menu.add(current_model.mesh, "name").name("Model's Name")
+          .onChange(function(event) {
+      
+          }).onFinishChange(function(event) {
+      
+          }); 
+          // start
+          let sliderPosX = current_menu.add(current_mesh.position, "x").min(-5).max(5).step(0.5).setValue(current_model.initial_x).name("X").listen().onChange(function(value) {
+      
+          });
+          let sliderRotY = current_menu.add(current_model, "rotY").min(-180).max(180).step(10).setValue(0).name("Y (deg)").listen().onChange(function(value) {
+              current_mesh.rotation.y = current_model.rotY * Math.PI / 180;
+          });;
+      
+          let testWireframe = current_menu.add(current_mesh.material, "wireframe").setValue(true).name("Wireframe").onChange(function(value) {
+      
+          });
+          let testColors = current_menu.add(current_model, "defaultItem", current_model.listColors).name("Color List").onChange(function(item) {
+              current_mesh.material.color = new THREE.Color(current_model.defaultItem.toLowerCase());
+              current_model.colorPalette = [current_mesh.material.color.r * 255, current_mesh.material.color.g * 255, current_mesh.material.color.b * 255];
+          });
+          let testPalette = current_menu.addColor(current_model, "colorPalette").name("Color Palette").listen().onChange(function(color) {
+              current_mesh.material.color = new THREE.Color(color[0]/255, color[1]/255, color[2]/255);
+          });
+          let btnRotHome = current_menu.add(current_model, "default_params").name("Reset default values").onChange(function(event) {
+              
+          });
+      }
+      // rotMenu.open();
+      gui.close()
+      
+      // STATS
+      this.stats = new Stats();
+      this.stats.showPanel(0); // FPS
+      document.body.appendChild(this.stats.dom);
 
-        });
-
-        // Appearance Menu
-        let appearMenu = gui.addFolder("Model's Appearance Menu");
-        let chbWireframe = appearMenu.add(this.mesh.material, "wireframe").setValue(true).name("Wireframe").onChange(function(value) {
-
-        });
-
-        var params = {
-            modelcolor: "#ff0000",
-            direction: "Derecha",
-            forma: "Cubo"
-        };
-
-        // Color Menu
-        let colorMenu = gui.addFolder("Color del cubo");
-
-        let chColor = colorMenu.addColor(params, 'modelcolor').name('Model Color').onChange(function() {
-                // this.mesh.material.color.set = "red"
-                console.log(this.mesh.material.color);
-                // this.mesh.color.set(params.modelcolor)
-                // materialmodel.color.set(params.modelcolor);
-        });
-
-        // Movimiento
-        let movimientoMenu = gui.addFolder("Movimiento del cubo");
-        let opMovimiento = movimientoMenu.add(params, 'direction', ['Derecha', 'Izquierda']).name("Dirección").onChange(function(value) {
-            alert(value);   
-            this.mesh.rotation.y = this.mesh.rotation.y
-        });
-
-        // Forma
-        let formaMenu = gui.addFolder("Forma del cubo");
-        let opForma = formaMenu.add(params, 'forma', ['Cubo', 'Esfera', 'Piramide']).name("Forma").onChange(function(value) {
-
-        });
-
-
-        rotMenu.open();
-        gui.close()
-        
-        // STATS
-        this.stats = new Stats();
-        this.stats.showPanel(0); // FPS
-        document.body.appendChild(this.stats.dom);
-
-        // ANIMATION
-        this.renderLoop();
+      // ANIMATION
+      this.renderLoop();
     },
     renderLoop(){
         this.stats.begin();
@@ -149,7 +194,7 @@ export default {
         requestAnimationFrame(this.renderLoop);
     },
     updateScene() {
-        this.mesh.rotation.y = this.mesh.rotation.y + 1 * Math.PI / 180;
+        // this.mesh.rotation.y = this.mesh.rotation.y + 1 * Math.PI / 180;
     }
   },
   mounted() {
